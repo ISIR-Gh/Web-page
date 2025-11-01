5a360438ee86d72a0777fbf746cf59b7b74ea065
Kenn12222 <ashartey2@gmail.com>
2025-11-01 11:01:04 +0000

    ken

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vue from "@astrojs/vue";
 
 export default defineConfig({
   // site: '',
-  //base: '/Web-page',
+  base: "/Web-page",
   integrations: [vue()],
   prefetch: {
     defaultStrategy: "viewport",
